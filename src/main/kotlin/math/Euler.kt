89ca0f23a27b4addbc93654d4b8a85ff7699e899
damian.mierzwinski <damian.mierzwinski@wundermanthompson.com>
2022-01-22 14:02:59 +0100

    Math - GCD

--- src/main/kotlin/math/Euler.kt
+++ src/main/kotlin/math/Euler.kt
@@ -2,22 +2,33 @@ package math
 
 class Euler {
     fun gcd(a: Int, b: Int): Int {
-        var greater = Math.max(a,b)
-        var smaller = Math.min(a,b)
+        var greater = Math.max(a, b)
+        var smaller = Math.min(a, b)
         var field = greater * smaller
-        while(field > 0) {
-            val smallerSquare = smaller*smaller
+        while (field > 0) {
+            val smallerSquare = smaller * smaller
             field -= smallerSquare
-            if(field == 0) return smaller
-            if(field >= smallerSquare){
+            if (field == 0) return smaller
+            if (field >= smallerSquare) {
                 greater = greater - smaller
-            }
-            else {
+            } else {
                 val tmp = smaller
                 smaller = greater - smaller
                 greater = tmp
             }
         }
         throw Exception()
+    }
+
+    fun gcd2(a: Int, b: Int): Int {
+        val greater = Math.max(a, b)
+        val smaller = Math.min(a, b)
+        return orderedGcd(smaller, greater)
+    }
+
+    tailrec fun orderedGcd(smaller: Int, greater: Int): Int {
+        val remainder = greater % smaller
+        if (remainder == 0) return smaller
+        return orderedGcd(remainder, smaller)
     }
 }
--- src/test/kotlin/math/EulerTest.kt
+++ src/test/kotlin/math/EulerTest.kt
@@ -7,10 +7,10 @@ import kotlin.test.Test
 internal class EulerTest {
     @Test
     fun `gcd test`() {
-        assertEquals(5, Euler().gcd(5,5))
-        assertEquals(5, Euler().gcd(10,5))
-        assertEquals(1, Euler().gcd(10,1))
-        assertEquals(1, Euler().gcd(13,6))
-        assertEquals(3, Euler().gcd(15,9))
+        assertEquals(5, Euler().gcd2(5, 5))
+        assertEquals(5, Euler().gcd2(10, 5))
+        assertEquals(1, Euler().gcd2(10, 1))
+        assertEquals(1, Euler().gcd2(13, 6))
+        assertEquals(12, Euler().gcd2(214748364, 21312324))
     }
 }
