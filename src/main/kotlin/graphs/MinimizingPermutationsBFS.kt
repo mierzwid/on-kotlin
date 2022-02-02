d3fa9c3879a558620df275bdef71f727a9ce91ae
damian.mierzwinski <damian.mierzwinski@wundermanthompson.com>
2022-02-02 21:47:45 +0100

    Graphs - Min Operations

--- src/main/kotlin/graphs/MinimizingPermutationsBFS.kt
+++ src/main/kotlin/graphs/MinimizingPermutationsBFS.kt
@@ -5,14 +5,12 @@ import kotlin.collections.HashSet
 
 fun minOperations(list: List<Int>): Int {
     val sorted = list.sorted()
-    if (list == sorted) return 0
     val queue = LinkedList<Pair<List<Int>, Int>>()
     val visited = HashSet<List<Int>>()
     queue.add(list to 0)
     visited.add(list)
     while (queue.isNotEmpty()) {
         val (current, count) = queue.poll()
-        println("$count: $current")
         if (current == sorted) return count
         for (i in 0 until current.lastIndex) {
             for (j in (i + 1)..current.lastIndex) {
